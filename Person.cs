ee32723947af995b64783e156498a092cae96974
ahmedhh33 <ahmedhh3633@gmail.com>
2023-07-03 00:18:16 +0400

    programs

--- Person.cs
+++ Person.cs
@@ -30,14 +30,27 @@ namespace Componsation_and_inheritance
 
         public string StudentId { get; set; }
         public string Grad { get; set; }
+        List <Course> StudentCourses { get; set; }
         public Student(string Name, int Age, string Address, string StudentId, string grad) : base(Name, Age, Address)
         {
             this.StudentId = StudentId;
             this.Grad = grad;
+            StudentCourses = new List <Course>();
         }
-        public string GetStudentInfo()
+        public void GetStudentInfo()
         {
-            return $" Person Name: {this.Name} Age : {this.Age} Adress : {this.Address} StufentID : {this.StudentId} Grad : {this.Grad} ";
+            Console.WriteLine(" Students Details");
+            Console.WriteLine($" Student Name: {this.Name} || Age : {this.Age} || Adress : {this.Address} || StufentID : {this.StudentId} || Grad : {this.Grad} ");
+            Console.WriteLine("Corses that student enrolled in it are ");
+            foreach(Course course in StudentCourses) 
+            {
+                Console.WriteLine(course.Title);
+            }
+            Console.WriteLine();
+        }
+        public void EnrollingStudentInCourses(Course InrolCourse)
+        {
+            StudentCourses.Add(InrolCourse);
         }
     }
 
@@ -46,15 +59,25 @@ namespace Componsation_and_inheritance
 
 
         public string TeacherId { get; set; }
-        public string Subjects { get; set; }
-        public Teacher(string Name, int Age, string Address, string TeacherId, string Subjects) : base(Name, Age, Address)
+        public List <string> TeacherSubjects { get; set; }
+        public Teacher(string Name, int Age, string Address, string TeacherId, List<string> TeacherSubjects) : base(Name, Age, Address)
         {
             this.TeacherId = TeacherId;
-            this.Subjects = Subjects;
+            this.TeacherSubjects = TeacherSubjects;
         }
-        public string GetTeacherInfo()
+        public void GetTeacherInfo()
         {
-            return $" Person Name: {this.Name} Age : {this.Age} Adress : {this.Address} StufentID : {this.TeacherId} Grad : {this.Subjects} ";
+            Console.WriteLine( $" Teacher Name: {this.Name} || Age : {this.Age} || Adress : {this.Address} || StufentID : {this.TeacherId} ");
+            Console.WriteLine("The Subjects that assigned to teacger are : ");
+            foreach(string assignedsubject in TeacherSubjects)
+            {
+                Console.WriteLine(assignedsubject);
+            }
+            Console.WriteLine();
+        }
+        public void AssignSubject(List<string> subjects)
+        {
+            TeacherSubjects=subjects;
         }
     }
 
@@ -63,14 +86,25 @@ namespace Componsation_and_inheritance
         public string CourseId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        Teacher TeacherId { get; set; }
+        public Teacher CourseTeacher { get; set; }//if the parameter is class dosenot need to be added to constructoor
 
-        public Course (string CourseId, string Title, string Description, Teacher TeacherId)
+        public Course (string CourseId, string Title, string Description)
         {
             this.CourseId = CourseId;
             this.Title = Title;
             this.Description = Description;
+        }
 
+        public void GetCourseinfo()
+        {
+            Console.WriteLine($"CoueseID: {this.CourseId} || Title: {this.Title} || Course Descriptions: {this.Description} ");
+            Console.WriteLine("Assigned Teacher: " + (CourseTeacher != null ? CourseTeacher.Name : "Not assigned"));
+            Console.WriteLine();
+        }
+
+        public void AssignTeacherToCourse(Teacher assigneteacher)
+        {
+            CourseTeacher=assigneteacher;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //entring studens objects
+            Student std1 = new Student("Ahmed",22,"Musalla","u11223","C+");
+            Student std2 = new Student("Saleh", 33, "AL-Aamirate","s12321","A");
+            //entring teather objects
+            Teacher teacher1 = new Teacher("Alkindy",65,"Canada","et3443",new List<string> { "Islamic Culture", "Arabic1","Filasufic"});//composation
+            Teacher teacher2 = new Teacher("Hassan",34,"Matrah","rt65654",new List<string> { "EM1","EM2", "High Voltage" });
+            // entring coureses
+            Course course1 = new Course("ECE345", "Higt Voltage", "Electrical Power advanciced");
+            Course course2 = new Course("ARE356","Isalmic Culture", "Islamic cultur in oman ");
+
+            std1.GetStudentInfo();//printing student information
+            std2.GetStudentInfo();
+            Console.WriteLine("********");
+
+            teacher1.GetTeacherInfo();//printing teacher info
+            teacher2.GetTeacherInfo();
+            Console.WriteLine("********");
+
+            course1.GetCourseinfo();//Displaying course details
+            course2.GetCourseinfo();
+            Console.WriteLine("********");
+
+            std1.EnrollingStudentInCourses(course1 );// Enrolling students in courses
+            std1.EnrollingStudentInCourses(course2);
+            std2.EnrollingStudentInCourses(course2 );
+
+            Console.WriteLine("printing student information after enroliing in courses");
+            std1.GetStudentInfo();//printing student information after enroliing in courses
+            std2.GetStudentInfo();
+            Console.WriteLine("********");
+
+            course1.AssignTeacherToCourse(teacher2 );// Assigning teachers to courses
+            course2.AssignTeacherToCourse(teacher1 );
+
+            Console.WriteLine("Displaying course details after assigning teachers to them");
+            course1.GetCourseinfo();//Displaying course details
+            course2.GetCourseinfo();
+            Console.WriteLine("********");
+
+
         }
     }
 }
